3765fc1752561aabf0da07dcf37ba2d4f1f1a315
gk622 <gk622@york.ac.uk>
2017-05-10 12:54:39 +0100

    Update vislat.py

--- vislat.py
+++ vislat.py
@@ -1,8 +1,7 @@
-GlowScript 2.4 VPython
 import numpy as np
 from visual import *
 
-atom, co_x, co_y, co_z = np.loadtxt("data.txt", unpack= true) e
+atom, co_x, co_y, co_z = np.loadtxt("copy_me_into_CASTEP.dat", unpack= true)
 
 N_atom = len(atom)
 
@@ -10,14 +9,14 @@ for i in range (1, N_atom):
 
   if atom(i) in ["O","o"]:
       colour = "green"
-  else if atom(i)  in ["Mg","mg"]:
+  elif atom(i)  in ["Mg","mg"]:
       colour = "red"
-  else
+  else:
       colour = "blue"
       
-   po_x = co_x(i)*4.2
-   po_y = co_y(i)*(len(atoms)/8.0)
-   po_z = co_z(i)*4.2
+  po_x = co_x(i)*4.2
+  po_y = co_y(i)*(len(atoms)/8.0)
+  po_z = co_z(i)*4.2
   
 
   sphere(pos = vector(po_x,po_y,po_z), radius = 0.3, color = color.colour)
